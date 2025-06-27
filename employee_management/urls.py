8754b8cb183dd68ca5e19e70b08c94a860fb97f1
codeview-ammar <a1221amar@gmail.com>
2025-06-27 22:46:12 +0300

    add

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns  # Import i18n_patterns
 from django.shortcuts import redirect
+from django.views.i18n import set_language
 
 def redirect_to_dashboard(request):
     if request.user.is_authenticated:
@@ -23,6 +24,7 @@ urlpatterns += i18n_patterns(
     path('accounts/', include('accounts.urls')),
     path('employees/', include('employees.urls')),
     path('', include('employees.urls')),  # Redirect root to employees
+    path('set_language/', set_language, name='set_language'),
 )
 
 if settings.DEBUG:
